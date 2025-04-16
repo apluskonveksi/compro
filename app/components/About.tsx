bf168cf7afe3f1a935399b54d46f62fcb36f06b7
apluskonveksi <hello.apluskonveksi@gmail.com>
2025-04-16 17:15:59 +0700

    add about text

---
+++ app/components/About.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4'>
+        <div className='antialiased'>
+          <p>Aplus Konveksi adalah mitra terpercaya dalam dunia konveksi yang berkomitmen tinggi untuk memberikan hasil produksi yang tidak hanya sesuai pesanan, tetapi juga unggul dalam hal kualitas dan ketepatan waktu. Kami memahami bahwa setiap produk yang kami hasilkan mencerminkan identitas brand klien, oleh karena itu kami selalu menjunjung tinggi standar mutu dalam setiap proses produksi.</p>
+          <p>Kami didukung oleh tim profesional yang ahli di bidangnya. Tim teknis kami terdiri dari tenaga potong kain, penjahit, hingga quality control (QC) yang telah melalui proses seleksi ketat dan mendapatkan pelatihan secara berkala untuk menjamin hasil yang presisi dan rapi. Tidak hanya itu, tim manajemen kami—yang mencakup desainer, konten kreator, tim pemasaran, admin, serta keuangan—juga merupakan individu-individu berpengalaman dan berdedikasi untuk mendukung kelancaran setiap proyek secara menyeluruh.</p>
+          <p>Seluruh proses produksi Aplus Konveksi berada di bawah naungan Aplus Multi Kreasi, sebuah perusahaan yang bergerak di bidang industri kreatif dan manufaktur. Dengan sistem kerja yang terstruktur dan diawasi melalui pendampingan sesuai standar industri, kami memastikan setiap tahapan produksi—mulai dari perencanaan desain, pemilihan bahan, proses produksi hingga pengemasan—berjalan dengan profesional, efisien, dan terkurasi dengan baik.</p>
+          <p> Komitmen kami tidak hanya berhenti pada hasil akhir yang memuaskan, namun juga pada pelayanan yang komunikatif, fleksibel, dan mengedepankan kebutuhan klien. Dengan semangat kolaborasi dan inovasi, Aplus Konveksi siap menjadi bagian dari kesuksesan brand Anda melalui produk-produk yang berkualitas dan terpercaya.</p>
+        </div>
+        <Image src="" alt="Aplus Konveksi" width={100} height={75} priority={true} className='object-cover w-full' />
+      </div>
+    </div>
+  )
+}
+
+export default About
