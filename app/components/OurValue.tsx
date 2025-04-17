b6cfae5684f8302aec6ca8c21ec935cf7211f300
apluskonveksi <hello.apluskonveksi@gmail.com>
2025-04-17 11:23:40 +0700

    edit typography

--- app/components/OurValue.tsx
+++ app/components/OurValue.tsx
@@ -7,9 +7,11 @@ const OurValue = () => {
     <div className='w-full flex flex-col md:flex-row items-center gap-4 justify-center pb-8'>
       {Services.map((service, index) => (
         <div key={index} className='w-full border border-gray-300 p-4 rounded-lg shadow-md flex flex-col items-start gap-4'>
-          <service.icon className='text-red-500' size={42} />
-          <h1 className='text-md md:text-lg lg:text-xl uppercase font-bold'  >{service.title}</h1>
-          <p>{service.description}</p>
+          <div className='space-y-1'>
+            <service.icon className='text-red-500' size={42} />
+            <h1 className='text-md md:text-lg lg:text-xl uppercase font-bold'  >{service.title}</h1>
+          </div>
+          <p className='text-[#333] leading-relaxed'>{service.description}</p>
         </div>
       ))}
 
--- app/components/ProblemSolved.tsx
+++ app/components/ProblemSolved.tsx
@@ -5,17 +5,17 @@ import React from 'react'
 const ProblemSolved = () => {
   return (
     <>
-      <div className='w-full h-full bg-[#1A202D] pt-8 px-4 relative overflow-hidden'>
-        <div className='w-full flex flex-col sm:flex-row items-end sm:items-start justify-evenly gap-4 '>
+      <div className='w-full h-full bg-[#1A202D] pt-8 px-4 relative overflow-hidden my-8 sm:my-14'>
+        <div className='w-full flex flex-col sm:flex-row items-end sm:items-start justify-evenly gap-4 py-8 sm:py-14 '>
           <div>
-            <h1 className="text-2xl md:text-4xl text-white">Kesulitan dalam <span className='text-red-400 font-semibold'>Pengadaan Seragam yang Ideal ?</span></h1>
+            <h1 className="text-2xl md:text-4xl text-white">Kesulitan dalam <span className='text-red-500 font-semibold'>Pengadaan Seragam yang Ideal ?</span></h1>
             <div className='py-6 flex flex-col gap-4'>
               <div>
                 <div className='flex items-start gap-2 w-full'>
                   <X className='text-red-400 w-5 h-5 bg-white p-0.5 rounded-full mt-1  ' />
                   <div className='w-full max-w-xl space-y-1.5'>
-                    <h1 className='text-white font-semibold text-lg'>Pengiriman Tidak Tepat Waktu</h1>
-                    <p className='text-gray-100'>Anda membutuhkan seragam yang siap digunakan untuk acara penting, namun proses produksi dan pengirimannya sering kali melewati tenggat yang telah ditentukan.</p>
+                    <h1 className='text-white font-semibold text-md sm:text-lg'>Pengiriman Tidak Tepat Waktu</h1>
+                    <p className='text-gray-100 text-sm sm:text-md md:text-lg leading-relaxed'>Anda membutuhkan seragam yang siap digunakan untuk acara penting, namun proses produksi dan pengirimannya sering kali melewati tenggat yang telah ditentukan.</p>
                   </div>
                 </div>
               </div>
@@ -23,8 +23,8 @@ const ProblemSolved = () => {
                 <div className='flex items-start gap-2 w-full'>
                   <X className='text-red-400 w-5 h-5 bg-white p-0.5 rounded-full mt-1 ' />
                   <div className='w-full max-w-xl space-y-1.5'>
-                    <h1 className='text-white font-semibold text-lg'>Kualitas yang Tidak Konsisten</h1>
-                    <p className='text-gray-100'>Seragam yang diterima tidak selalu sesuai dengan standar yang diharapkan—baik dari segi bahan, jahitan, maupun hasil akhir.</p>
+                    <h1 className='text-white font-semibold text-md sm:text-lg'>Kualitas yang Tidak Konsisten</h1>
+                    <p className='text-gray-100 text-sm sm:text-md md:text-lg leading-relaxed'>Seragam yang diterima tidak selalu sesuai dengan standar yang diharapkan—baik dari segi bahan, jahitan, maupun hasil akhir.</p>
                   </div>
                 </div>
               </div>
@@ -32,18 +32,19 @@ const ProblemSolved = () => {
                 <div className='flex items-start gap-2 w-full'>
                   <X className='text-red-400 w-5 h-5 bg-white p-0.5 rounded-full mt-1  ' />
                   <div className='w-full max-w-xl space-y-1.5'>
-                    <h1 className='text-white font-semibold text-lg'>Desain Kurang Merepresentasikan Brand</h1>
-                    <p className='text-gray-100'>Desain seragam belum mampu merefleksikan identitas, visi, atau nilai dari perusahaan Anda secara optimal.</p>
+                    <h1 className='text-white font-semibold text-md sm:text-lg'>Desain Kurang Merepresentasikan Brand</h1>
+                    <p className='text-gray-100 text-sm sm:text-md md:text-lg leading-relaxed'>Desain seragam belum mampu merefleksikan identitas, visi, atau nilai dari perusahaan Anda secara optimal.</p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <Image src="/model-t.png" alt="model-image" width={250} height={250} className='-bottom-4 relative' priority={true} />
+          <Image src="/model-t.png" alt="model-image" width={250} height={250} className='-bottom-10 sm:-bottom-14 relative' priority={true} />
         </div>
       </div>
       <div className='py-8'>
-        <h1>Tenang, Aplus Konveksi Solusinya</h1>
+        <h1 className="text-2xl md:text-4xl text-[#1A202D]">Yups, <span className='text-red-500 font-semibold'> Aplus Konveksi Solusinya!</span></h1>
+
       </div>
     </>
   )
