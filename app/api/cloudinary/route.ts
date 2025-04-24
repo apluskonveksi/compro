b066955df4361bb43e6d592db054a04e7f36518b
apluskonveksi <hello.apluskonveksi@gmail.com>
2025-04-24 10:09:58 +0700

    change key

--- app/api/cloudinary/route.ts
+++ app/api/cloudinary/route.ts
@@ -6,9 +6,9 @@ export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const activeButton = searchParams.get("activeButton"); // Ambil nilai activeButton dari query parameter
 
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-  const apiSecret = process.env.NEXT_PUBLIC_API_SECRET;
-  const cloudName = process.env.NEXT_PUBLIC_CLOUD_NAME;
+  const apiKey = process.env.API_KEY;
+  const apiSecret = process.env.API_SECRET;
+  const cloudName = process.env.CLOUD_NAME;
   const auth = Buffer.from(`${apiKey}:${apiSecret}`).toString("base64");
 
   try {
